bcbb10ea0ae0ddc8e2f76f87882f2c77a16c0e0b
Ethan Denny <ethandenny@protonmail.com>
2024-04-29 22:38:43 -0230

    Fix config?

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ethandenny.github.io/music-reviews/",
+  base: "/music-reviews/",
 })
